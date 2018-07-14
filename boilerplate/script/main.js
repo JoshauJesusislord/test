bbd8bcb0d998fa8ede529af361810870b2b7d868
Your Name <joshuajesusislord@gmail.com>
2018-07-14 10:37:44 +0100

    first commit

---
+++ boilerplate/script/main.js
@@ -0,0 +1,51 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = "Oh... " + "Hello " + "world!";
+//
+// kvjlkvmc
+// hxjdklxlckx
+// djhfkdnl
+// kjncvjc
+// nl
+// odflsk
+
+// var iceCream = 'chocolate' ;
+// if (iceCream === 'chocolate' ) {
+//   alert('Yay, I love chocolate ice cream!');    
+// } else {
+//   alert('Awwww, but chocolate is my favorite...');    
+// }
+
+
+
+// function multiply(num1,num2) {
+//     var result = num1 * num2;
+//   alert (result);
+//     return result;
+//   }
+//   multiply(4,7);
+// multiply(20,20);
+// multiply(0.5,3);
+
+// // document.querySelector('html').onclick = function() {
+// //     alert('OUCH! STOP POKING ME.');
+
+// }
+
+
+document.querySelector('body').onscroll = function() {
+    alert('OUCH! STOP POKING ME.');
+
+}
+
+
+// var myImage = document.querySelector('img');
+
+// myImage.onclick = function() {
+//     var mySrc = myImage.getAttribute('src');
+//     if(mySrc === 'images/firefox-icon.png') {
+//       myImage.setAttribute ('src','images/firefox2.png');
+//     } else {
+//       myImage.setAttribute ('src','images/brothers.jpg');
+//     }
+
+// }
